7ae9ef5180c108e9e3569f3ba0aeda5df4789f28
Nicolás Fernando Miari <nicolasmiari@gmail.com>
2015-11-30 17:47:45 +0900

    Added workaround demo case for Class with Empty Description Text.

---
+++ JazzyTestBed/JazzyTestBed/EmptyDescriptionZeroMargin.swift
@@ -0,0 +1,21 @@
+//
+//  EmptyDescriptionZeroMargin.swift
+//  JazzyTestBed
+//
+//  Created by Nicolás Fernando Miari on 2015/11/30/.
+//  Copyright © 2015 Nicolás Miari. All rights reserved.
+//
+
+import Foundation
+
+/**
+- since: 0.1
+- author: Nicolás Miari.
+- todo: If the description is empty but all the documentation comment keywords 
+ and content start at the very beginning of each line (no leading space), then 
+ the issue does not occur.
+ */
+class EmptyDescriptionZeroMargin
+{
+    
+}
